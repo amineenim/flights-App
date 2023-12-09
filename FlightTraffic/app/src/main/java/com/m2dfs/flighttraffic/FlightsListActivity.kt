f2606de3ac042cc8f7aab3318f68e79f413de0bd
amineenim <aminemaourid1995@gmail.com>
2023-12-10 00:04:13 +0100

    modifying FlightsListActivity to set the layout on flight_list_fragment

--- FlightTraffic/app/src/main/java/com/m2dfs/flighttraffic/FlightsListActivity.kt
+++ FlightTraffic/app/src/main/java/com/m2dfs/flighttraffic/FlightsListActivity.kt
@@ -3,7 +3,6 @@ package com.m2dfs.flighttraffic
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
@@ -13,7 +12,7 @@ class FlightsListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.flights_list)
+        setContentView(R.layout.flight_list_fragment)
 
         this.viewModel = ViewModelProvider(this)[FlightsListViewModel::class.java]
         Log.i("debug", intent.getLongExtra("BEGIN", 0).toString())
@@ -26,7 +25,8 @@ class FlightsListActivity : AppCompatActivity() {
         this.viewModel.doRequest()
         Log.i("heere", "passed here !")
 
-        val isTablet = findViewById<FragmentContainerView>(R.id.fragment_map_container) != null
+        val isTablet = false
+            //findViewById<FragmentContainerView>(R.id.fragment_map_container) != null
         viewModel.getClickedFlightLiveData().observe(this, Observer {
             // Afficher le bon vol
             Log.i("haha", "passed here ")
